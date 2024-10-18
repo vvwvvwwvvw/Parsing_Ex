c6bca9453803eedfd0d5facde62fd42186d5a7b3
남정욱 <wjddnr3304@gmail.com>
2024-10-18 10:39:16 +0900

    초기 커밋.

---
+++ Parsing_Ex/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Parsing_Ex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Pares 메소드 예
+            string intStrinf = "123";
+            int parsedInt = int.Parse(intStrinf);
+            Console.WriteLine($"Parse 로 변환된 int 값: {parsedInt}");
+
+            // TryParse 메소드 예
+            string doubleString = "123.45";
+            double parsedDouble;
+            bool isDoubleParseSuccessful = double.TryParse(doubleString, out parsedDouble);
+            Console.WriteLine(isDoubleParseSuccessful ? $"파싱된 double값 : {parsedDouble}" : "double로 파싱 실패");
+        }
+    }
+}
